c439e8d77bcb2520d0ef7aae24f60cb994e69e66
Chaudhary Zohaib Yousuf <156688003+chaudharyZohaib@users.noreply.github.com>
2024-03-02 08:20:52 +0500

    Arithmetic Operators

---
+++ java5.js
@@ -0,0 +1,39 @@
+        // Arithmetic operators
+let a = 10;
+let b = 5;
+
+
+
+
+
+            // Addition
+let additionResult = a + b;
+console.log("Addition:", additionResult);
+
+            // Subtraction
+let subtractionResult = a - b;
+console.log("Subtraction:", subtractionResult);
+
+            // Multiplication
+let multiplicationResult = a * b;
+console.log("Multiplication:", multiplicationResult);
+
+             // Division
+let divisionResult = a / b;
+console.log("Division:", divisionResult);
+
+           // Modulus (Remainder)
+let modulusResult = a % b;
+console.log("Modulus:", modulusResult);
+
+            // Exponentiation
+let exponentiationResult = a ** b;
+console.log("Exponentiation:", exponentiationResult);
+
+             // Increment
+let incrementA = ++a;
+console.log("Increment a:", incrementA);
+
+              // Decrement
+let decrementB = --b;
+console.log("Decrement b:", decrementB);
